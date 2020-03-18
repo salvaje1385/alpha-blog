bb13ab7e986a6a2bdd3bb6ecf4452c7931349a9d
Salvador Ruiz <sruiz1385@hotmail.com>
2020-03-17 22:46:41 -0600

    add association between user and articles, and add debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
 
     if @article.save
       flash[:success] = "Article was successfully created"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-	 validates :username, presence: true,
+	has_many :articles
+	before_save { self.email = email.downcase }
+	validates :username, presence: true,
 				uniqueness: { case_sensitive: false },
 				length: { minimum: 3, maximum: 25 }
 
